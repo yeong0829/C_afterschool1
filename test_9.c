540a6c42d75af2b3c8bb995f24cc8bd7c9050f74
yeong431 <w2017@e-mirim.hs.kr>
2021-12-05 02:13:24 +0900

    .

---
+++ test_9.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS    
+#include <stdio.h>
+
+int mai(void) {
+	int num[4];
+	int max = 0, min = 0;
+	for (int i = 0; i <= num[4]; i++) 
+		printf("정수 입력: ");
+		scanf_s("%d", num[0]);
+	
+
+	//printf("최대값: %d, 최소값: %d\n", max, min, get(num[]));
+
+	return 0;
+}
+
+//int get(int *num) {
+	//int max=0, min=0;
+	//for (int i = 0; i < 4; i++)
+		//max += num[i];
+	//for (int i = 0; i < 4; i++)
+		//min -= num[i];
+	//return max, min;
+//}
+
+
+	
